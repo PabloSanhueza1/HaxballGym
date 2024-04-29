6ea7eafa76e1980c6b50907b30029f7c5c0bcfc1
Pablo Sanhueza <Sanhueza.pem@gmail.com>
2024-04-29 17:08:24 -0400

    add bot

--- example.py
+++ example.py
@@ -1,42 +1,67 @@
-import time
+from dataclasses import dataclass
+
+from ursina import Keys, held_keys
 
 from ursinaxball import Game
-
-import haxballgym
+from ursinaxball.common_values import BaseMap, TeamID
+from ursinaxball.modules import GameScore, PlayerHandler, ChaseBot
 
 game = Game(
     folder_rec="./recordings/",
-    enable_renderer=True,
     enable_vsync=True,
+    stadium_file=BaseMap.CLASSIC,
 )
-env = haxballgym.make(game=game)
+game.score = GameScore(time_limit=3, score_limit=3)
+tick_skip = 2
 
-ep_reward = 0
+bot_blue = ChaseBot(tick_skip)
+
+player_red = PlayerHandler("P1", TeamID.RED)
+player_blue = PlayerHandler("P2", TeamID.BLUE, bot=bot_blue)
+game.add_players([player_red, player_blue])
+
+
+@dataclass
+class InputPlayer:
+    left: list[str]
+    right: list[str]
+    up: list[str]
+    down: list[str]
+    shoot: list[str]
+
+
+input_player = InputPlayer(
+    left=[Keys.left_arrow],
+    right=[Keys.right_arrow],
+    up=[Keys.up_arrow],
+    down=[Keys.down_arrow],
+    shoot=["x"],
+)
+
+
+def action_handle(actions_player_output: list[int], inputs_player: InputPlayer):
+    actions_player_output = [0, 0, 0]
+    for key, value in held_keys.items():
+        if value != 0:
+            if key in inputs_player.left:
+                actions_player_output[0] -= 1
+            if key in inputs_player.right:
+                actions_player_output[0] += 1
+            if key in inputs_player.up:
+                actions_player_output[1] += 1
+            if key in inputs_player.down:
+                actions_player_output[1] -= 1
+            if key in inputs_player.shoot:
+                actions_player_output[2] += 1
+    return actions_player_output
+
 
 while True:
     save_rec = False
-    if abs(ep_reward) > 1:
-        save_rec = True
-    obs = env.reset(save_recording=save_rec)
-    obs_1 = obs[0]
-    obs_2 = obs[1]
+    game.reset(save_recording=save_rec)
     done = False
-    steps = 0
-    ep_reward = 0
-    t0 = time.time()
+    actions = [[0, 0, 0], [0, 0, 0]]
     while not done:
-        actions_1 = env.action_space.sample()
-        actions_2 = env.action_space.sample()
-        actions = [actions_1, actions_2]
-        new_obs, reward, done, state = env.step(actions)
-        ep_reward += reward[0]
-        obs_1 = new_obs[0]
-        obs_2 = new_obs[1]
-        steps += 1
-
-    length = time.time() - t0
-    print(
-        "Step time: {:1.5f} | Episode time: {:.2f} | Episode Reward: {:.2f}".format(
-            length / steps, length, ep_reward
-        )
-    )
+        actions[0] = action_handle(actions[0], input_player)
+        actions[1] = player_blue.step(game)
+        done = game.step(actions)
